5b457143a56464fd9f0d24f5c6ba48d8b726d9d0
Milad Raza <miladrazabhatti@gmail.com>
2021-10-03 16:14:13 +0500

    socket.io comments //

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const winston = require('winston');
 const express = require('express');
+// const socket = require("socket.io")
 const app = express();
 
 require('./startup/logging')();
@@ -8,4 +9,13 @@ require('./startup/db')();
 require('./startup/config')();
 
 const port = process.env.PORT || 5000
+
 app.listen(port, () => winston.info(`listing on port ${port}...`))
+
+// const server = app.listen(port, () => winston.info(`listing on port ${port}...`))
+
+// const io = socket(server);
+
+// io.on("connection",  (socket) => {
+//     console.log("Made socket connection", socket);
+// });
