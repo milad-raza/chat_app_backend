57e8cb82d536a3b3408da13715674b9b9daee33b
Milad Raza <miladrazabhatti@gmail.com>
2021-10-12 11:12:04 +0500

    helmet added to protect response headers & selected url's added to only allow CORS

--- startup/routes.js
+++ startup/routes.js
@@ -5,10 +5,23 @@ const auth = require('../routes/auth');
 const conversations = require('../routes/convertations')
 const messages = require('../routes/messages');
 const error = require('../middleware/error');
+const helmet = require('helmet');
 
 module.exports = function (app) {
-    app.use(cors())
-    app.use(express.json())  
+
+    const whitelist = ['https://mr-chat-app.web.app', 'http://localhost:3000']
+    const corsOptions = {
+        origin: function (origin, callback) {
+            if (whitelist.indexOf(origin) !== -1) {
+                callback(null, true)
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        }
+    }
+    app.use(express.json())
+    app.use(helmet())
+    app.use(cors(corsOptions))
     app.use('/api/users', users)
     app.use('/api/conversations', conversations)
     app.use('/api/messages', messages)
