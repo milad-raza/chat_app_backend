12325a112c981ef96675a028d5c812e21033317f
Milad Raza <miladrazabhatti@gmail.com>
2021-10-12 10:34:33 +0500

    CORS issue fixed using cors npm package

--- startup/routes.js
+++ startup/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const users = require('../routes/users');
 const auth = require('../routes/auth');
 const conversations = require('../routes/convertations')
@@ -6,6 +7,7 @@ const messages = require('../routes/messages');
 const error = require('../middleware/error');
 
 module.exports = function (app) {
+    app.use(cors())
     app.use(express.json())  
     app.use('/api/users', users)
     app.use('/api/conversations', conversations)
